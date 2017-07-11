2db8da45db6cd03d726621620bffcc805a3e3bd1
Pedro Paiva <pedrovsp109@gmail.com>
2017-07-10 23:48:21 -0300

    3D Sphere As Bowling Ball

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public float launchSpeed = 5f;
+
+	private Rigidbody rigidBody;
+	private AudioSource audioSource;
+
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+		audioSource = GetComponent<AudioSource> ();
+
+		Launch ();
+	}
+
+	public void Launch ()
+	{
+		rigidBody.velocity = new Vector3 (0f, 0f, launchSpeed);
+		audioSource.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
