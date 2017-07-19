18327d6dfa0255c5bae58cb84efb8c641aee0c4e
Pedro Paiva <pedrovsp109@gmail.com>
2017-07-19 14:38:04 -0300

    Finishing Our Control Code

--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -6,12 +6,25 @@ public class ActionMaster {
 
 	public enum Action {Tidy, Reset, EndTurn, EndGame};
 
-	//private int[] bowls = new int[21];
+	private int[] bowls = new int[21];
 	private int bowl = 1;
 
 	public Action Bowl (int pins) {
 
 		if (pins < 0 || pins > 10) 	{ throw new UnityException ("Invalid pins value!"); }
+
+		bowls [bowl - 1] = pins;
+
+		if (bowl == 21) {
+			return Action.EndGame;
+		}	
+
+		if (bowl >= 19 && Bowl21Awarded ()) {
+			bowl += 1;
+			return Action.Reset;
+		} else if (bowl == 20 && !Bowl21Awarded()) {
+			return Action.EndGame;
+		}
 
 		if (pins == 10) { 
 			bowl += 2;
@@ -27,5 +40,9 @@ public class ActionMaster {
 		}
 
 		throw new UnityException ("Not sure what action to return!");
+	}
+
+	bool Bowl21Awarded() {
+		return (bowls [19 - 1] + bowls[20 - 1] >= 10) ;
 	}
 }
