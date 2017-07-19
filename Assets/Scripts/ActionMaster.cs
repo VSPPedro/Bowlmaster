389beb94bcc61916c69c34e9b1f7d688c6aad271
Pedro Paiva <pedrovsp109@gmail.com>
2017-07-19 14:00:08 -0300

    Red > Green > Refactor

--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -6,12 +6,24 @@ public class ActionMaster {
 
 	public enum Action {Tidy, Reset, EndTurn, EndGame};
 
+	//private int[] bowls = new int[21];
+	private int bowl = 1;
+
 	public Action Bowl (int pins) {
 
-		if (pins < 0 || pins > 10) { throw new UnityException ("Invalid pins value!"); }
+		if (pins < 0 || pins > 10) 	{ throw new UnityException ("Invalid pins value!"); }
 
-		if (pins == 10) {
-			return Action.EndTurn;		
+		if (pins == 10) { 
+			bowl += 2;
+			return Action.EndTurn;
+		}
+
+		if (bowl % 2 != 0) { 
+			bowl += 1;
+			return Action.Tidy; 
+		} else if (bowl % 2 == 0) {
+			bowl += 1;
+			return Action.EndTurn;
 		}
 
 		throw new UnityException ("Not sure what action to return!");
