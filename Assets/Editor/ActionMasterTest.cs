6ee874fa82343773336803889be1667cb921516c
Pedro Paiva <pedrovsp109@gmail.com>
2017-07-19 12:24:49 -0300

    Making Your First Test Pass

---
+++ Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+[TestFixture]
+public class ActionMasterTest {
+
+	private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+
+	[Test]
+	public void T00PassingTest() {
+		Assert.AreEqual (2, 2);
+	}
+
+	[Test]
+	public void T01OneStrikeReturnsEndTurn() {
+		ActionMaster actionMaster = new ActionMaster ();
+		Assert.AreEqual (endTurn, actionMaster.Bowl(10));
+	}
+}
